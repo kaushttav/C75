271fc2de13898b54310ee2d3d16b853c205d1dd1
Kaushttav Karan <kaushttavkaran@gmail.com>
2021-03-27 11:34:54 +0530

    message

--- App.js
+++ App.js
@@ -1,15 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import { createAppContainer,createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import ReadStoryScreen from './screens/ReadStoryScreen';
+import WriteStoryScreen from './screens/WriteStoryScreen';
+import LogInScreen from './screens/LogInScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <AppContainer/>
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Read: {screen:ReadStoryScreen},
+  Write: {screen: WriteStoryScreen},
+ 
+ 
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      if(routeName === "Read"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40, height:40}}
+        />
+        )
+
+      }
+      else if(routeName === "Write"){
+        return(
+          <Image
+          source={require("./assets/write.png")}
+          style={{width:40, height:40}}
+        />)
+
+      }
+    }
+  })
+}
+);
+
+var SwitchNavigator = createSwitchNavigator({
+  LogInScreen:{screen:LogInScreen},
+  Home:{screen:TabNavigator},
+})
+const AppContainer =  createAppContainer(SwitchNavigator);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
